186f31e3f7fcb08b9a8bc96e511f2a8f5d265c60
Rakesh Sirvi <rsirvi797@gmail.com>
2021-08-24 14:26:57 +0530

    Updates

---
+++ precache-manifest.fe3bfb53efeaef73b84d980dc7b2641e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086e26d0f779eee6f7a4513bec93dd8f",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "934d15bc56cf43d30525",
+    "url": "/TodoList/static/css/main.19c647fe.chunk.css"
+  },
+  {
+    "revision": "3825de79f3829e3cdda0",
+    "url": "/TodoList/static/js/2.72b44912.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.72b44912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "934d15bc56cf43d30525",
+    "url": "/TodoList/static/js/main.c8c410a6.chunk.js"
+  },
+  {
+    "revision": "141e57522120cb2dabad",
+    "url": "/TodoList/static/js/runtime-main.d512a30b.js"
+  }
+]);
